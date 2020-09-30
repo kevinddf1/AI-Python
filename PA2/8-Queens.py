0da36c49e6addb83fb6b927d0c4f8717c209f2c6
kevinddf1 <46538840+kevinddf1@users.noreply.github.com>
2020-09-30 08:43:54 -0500

    setup pa2

---
+++ PA2/8-Queens.py
@@ -0,0 +1,32 @@
+import sys
+import math
+import time
+from queue import PriorityQueue
+
+
+# Author: Fan Ding
+# Email: ding0322@umn.edu
+# UMN CSCI 5511 AI
+# Prof: Andy Exley
+# Date: 09/30/2020 
+
+print("Welcome to 8-Queens Problem")
+
+
+#That uses hill climbing with steepest-ascent to attempt to solve the problem. 
+# Have your code detect a plateau or local maximum and give up if those are encountered.
+def hillclimb_sa():
+    pass
+
+
+#That implements first-choice hill climbing for the same problem.
+def hillclimb_fc():
+    pass
+
+#That implements simulated annealing for the problem. Choose your temperature and schedule.
+def sim_anneal():
+    pass
+
+
+with open('readme.txt', 'w') as f:
+    f.write("hello world!!!!!!!!!\n")
